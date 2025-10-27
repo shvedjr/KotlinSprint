bd34faf6c69c226fd10062f4e21c87a46101443c
shvedjr <shvedjr@gmail.com>
2025-10-27 13:49:51 +0300

    KS-1-3-task3

--- src/main/kotlin/lesson1_task3.kt
+++ src/main/kotlin/lesson1_task3.kt
@@ -1,19 +1,14 @@
 package org.example.lesson_1
 
 fun main() {
-    val totalOfOrders: Int = 75
-
-    val sendThankYouMessage: String = "Спасибо, что выбрали наш магазин!"
 
     val year: Int = 1961
-
     var hour: String = "09"
-
     var minute: String = "07"
 
     println(year)
-    //println(hour)
-    //println(minute)
+    println(hour)
+    println(minute)
 
     hour = "10:"
     print(hour)
