534e6816b94f2eeb1684149041425e064327add0
shvedjr <shvedjr@gmail.com>
2025-10-27 11:32:56 +0300

    Task 2 for verification

---
+++ src/main/kotlin/lesson1_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val totalOfOrders: Int = 75
+    val sendThankYouMessage: String = "Спасибо, что выбрали наш магазин!"
+
+    println(totalOfOrders)
+    println(sendThankYouMessage)
+
+    var numberOfEmployees: Int = 1999
+
+//    println(numberOfEmployees)
+    println(numberOfEmployees)
+
+}
