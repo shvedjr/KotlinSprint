eb1054c7f615416f3b62503693e84d5e579b2f57
shvedjr <shvedjr@gmail.com>
2025-10-28 19:57:53 +0300

    KS-2-5-task5

---
+++ src/main/kotlin/lesson2_task5.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_2
+
+import kotlin.math.pow
+
+fun main() {
+
+    val clientStartPeriod: Int = 70000
+    val creditPeriod: Int = 20
+    val percentPerYearCredit: Double = 16.7 / 100
+
+    val clientEndOfPeriod: Double = clientStartPeriod * (1+percentPerYearCredit / 1).pow(1*creditPeriod)
+
+    println("%.3f".format(clientEndOfPeriod))
+}
