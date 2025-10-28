7a1a570f0e70c5f0fa44d37d04b8625dbbfa28f5
shvedjr <shvedjr@gmail.com>
2025-10-28 19:17:52 +0300

    KS-2-4-task4

---
+++ src/main/kotlin/lesson2_task4.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val crystalOre: Short = 7
+    val metalOre: Short = 11
+
+    val buffCrystalOre: Double = crystalOre * 0.20
+    val buffMetalOre: Double = metalOre * 0.20
+
+    println(buffCrystalOre.toInt())
+    println(buffMetalOre.toInt())
+
+}
