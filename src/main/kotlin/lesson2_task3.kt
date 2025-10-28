4a21104460983b6c23e376670de14542c5726c54
shvedjr <shvedjr@gmail.com>
2025-10-28 17:07:15 +0300

    KS-2-3-task1

--- src/main/kotlin/lesson2_task3.kt
+++ src/main/kotlin/lesson2_task3.kt
@@ -1,5 +1,7 @@
 package org.example.lesson_2
 
+const val MINUTES_IN_HOUR = 60
+
 fun main() {
 
     val departureTimeHours: Int = 9
@@ -7,12 +9,12 @@ fun main() {
 
     val travelTime: Int = 457 //минут в пути
 
-    val totalDepartureMinutes: Int = departureTimeHours * 60 + departureTimeMinutes //Время отправления в минутах
+    val totalDepartureMinutes: Int = departureTimeHours * MINUTES_IN_HOUR + departureTimeMinutes
 
     val arrivalTotalMinutes: Int = totalDepartureMinutes + travelTime
 
-    val arrivalHours: Int = arrivalTotalMinutes / 60 //17 часов время прибытия
-    val arrivalMinutes: Int = arrivalTotalMinutes % 60 // остаток минут прибытия
+    val arrivalHours: Int = arrivalTotalMinutes / MINUTES_IN_HOUR
+    val arrivalMinutes: Int = arrivalTotalMinutes % MINUTES_IN_HOUR
 
   println("%02d:%02d".format(arrivalHours, arrivalMinutes))
 
