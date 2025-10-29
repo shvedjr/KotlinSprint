24bc2386c3750553c78a6aefae0ea25289e4347a
shvedjr <shvedjr@gmail.com>
2025-10-30 00:46:58 +0300

    KS-2-1-task1

--- src/main/kotlin/lesson2_task1.kt
+++ src/main/kotlin/lesson2_task1.kt
@@ -2,13 +2,13 @@ package org.example.lesson_2
 
 fun main() {
 
-    val allStudents: Double = 4.0
-    val student1: Short = 3
-    val student2: Short = 4
-    val student3: Short = 3
-    val student4: Short = 5
+    val allStudents: Short = 4
+    val student1: Double = 3.0
+    val student2: Double = 4.0
+    val student3: Double = 3.0
+    val student4: Double = 5.0
 
-    val averageValue: Double = (student1 + student2 + student3 + student4) / allStudents
+    val averageValue: Double = (student1 + student2 + student3 + student4) / 4.0
 
   println(averageValue)
 }
