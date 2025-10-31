de37570cc8c9ca4bd309704bc2de41beecfb3a10
shvedjr <shvedjr@gmail.com>
2025-11-01 00:10:10 +0300

    KS-3-5-task5 changed it using the "split" function

--- src/main/kotlin/lesson3_task5.kt
+++ src/main/kotlin/lesson3_task5.kt
@@ -4,11 +4,13 @@ fun main() {
 
     val nextMoveSring: String = "D2-D4;0"
 
-    val from: String = nextMoveSring.substring(0, 2)
-    val to: String = nextMoveSring.substring(3, 5)
-    val moveNumber: String = nextMoveSring.substring(6)
+    val allParts = nextMoveSring.split("-", ";")
 
-    println("Откуда:$from")
-    println("Куда:$to")
-    println("Номер хода:$moveNumber")
+    val from = allParts[0]
+    val to = allParts[1]
+    val moveNumber = allParts[2]
+
+    println("Откуда: $from")
+    println("Куда: $to")
+    println("Номер хода: $moveNumber")
 }
