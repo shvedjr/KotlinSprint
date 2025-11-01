4edda53ce9a889f197486826b5a2e99a0c42cb1c
shvedjr <shvedjr@gmail.com>
2025-11-01 19:14:46 +0300

    KS-4-5-task5

---
+++ src/main/kotlin/lesson4_task5.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_4
+
+const val SHIP_DAMAGE = false
+const val MIN_NUMBER_OF_CREW = 55
+const val MAX_NUMBER_OF_CREW = 70
+const val GOOD_WEATHER = true
+const val MIN_PROVISIONS_BOX = 50
+
+fun main() {
+
+    val shipDamage = true
+    val numberOfCrew = 58
+    val weather = true
+    val provisionsBox = 51
+
+println("Кораблю разрешено отправиться в плавание: ${(shipDamage == SHIP_DAMAGE || !shipDamage == SHIP_DAMAGE) && (numberOfCrew >= MIN_NUMBER_OF_CREW) && (numberOfCrew <= MAX_NUMBER_OF_CREW) && (weather == GOOD_WEATHER) && (provisionsBox > MIN_PROVISIONS_BOX)}")
+}
