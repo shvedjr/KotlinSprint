028057a6feb61c0a31bfb2b4aa7af3f85b9513ba
shvedjr <shvedjr@gmail.com>
2025-11-01 14:31:20 +0300

    KS-4-2-task2

---
+++ src/main/kotlin/lesson4_task2.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_4
+
+const val AVERAGE_WEIGHT_LOW = 35
+const val AVERAGE_WEIGHT_HIGH = 100
+const val AVERAGE_VOLUME = 100
+
+fun main() {
+
+    var checkCargoWeight = 20
+    var average = checkCargoWeight > AVERAGE_WEIGHT_LOW && checkCargoWeight <= AVERAGE_WEIGHT_HIGH
+    println("Груз №1 с весом 20кг не соответсвует категории 'Average': $average")
+
+    checkCargoWeight = 50
+    var checkVolume = 80
+    average = checkCargoWeight > AVERAGE_WEIGHT_LOW && checkCargoWeight <= AVERAGE_WEIGHT_HIGH && checkVolume < AVERAGE_VOLUME
+    println("Груз №2 с весом 50кг и объемом 80 литров соответсвует категории 'Average':$average")
+
+    checkVolume = 100
+    average = checkVolume < AVERAGE_VOLUME
+    println("Груз №3 с объемом 100л не соответсвует категории 'Average': $average")
+}
