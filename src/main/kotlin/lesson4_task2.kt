18d4aed7db918bc5f52ae478321382df20ca1c42
shvedjr <shvedjr@gmail.com>
2025-11-01 16:04:24 +0300

    KS-4-2-task1

--- src/main/kotlin/lesson4_task2.kt
+++ src/main/kotlin/lesson4_task2.kt
@@ -7,15 +7,10 @@ const val AVERAGE_VOLUME = 100
 fun main() {
 
     var checkCargoWeight = 20
-    var average = checkCargoWeight > AVERAGE_WEIGHT_LOW && checkCargoWeight <= AVERAGE_WEIGHT_HIGH
-    println("Груз №1 с весом 20кг не соответсвует категории 'Average': $average")
+    var checkVolume = 80
+    println("Груз с весом 20кг и объемом 80л соответсвует категории 'Average': ${checkCargoWeight > AVERAGE_WEIGHT_LOW && checkCargoWeight < AVERAGE_WEIGHT_HIGH && checkVolume <= AVERAGE_VOLUME}")
 
     checkCargoWeight = 50
-    var checkVolume = 80
-    average = checkCargoWeight > AVERAGE_WEIGHT_LOW && checkCargoWeight <= AVERAGE_WEIGHT_HIGH && checkVolume < AVERAGE_VOLUME
-    println("Груз №2 с весом 50кг и объемом 80 литров соответсвует категории 'Average':$average")
-
     checkVolume = 100
-    average = checkVolume < AVERAGE_VOLUME
-    println("Груз №3 с объемом 100л не соответсвует категории 'Average': $average")
+    println("Груз с весом 50кг и объемом 100л соответсвует категории 'Average': ${checkCargoWeight > AVERAGE_WEIGHT_LOW && checkCargoWeight < AVERAGE_WEIGHT_HIGH && checkVolume <= AVERAGE_VOLUME}")
 }
