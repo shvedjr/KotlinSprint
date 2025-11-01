6382937532826c1bb63823a9f826b9c874e53444
shvedjr <shvedjr@gmail.com>
2025-11-01 23:33:43 +0300

    KS-4-4-task4

--- src/main/kotlin/lesson4_task4.kt
+++ src/main/kotlin/lesson4_task4.kt
@@ -4,15 +4,12 @@ fun main() {
 
     val dayOfTraining = 5
 
-    var hands: Boolean = !(dayOfTraining % 2 == 0)
-    var legs: Boolean = dayOfTraining % 2 == 0
-    var back: Boolean = dayOfTraining % 2 == 0
-    var press: Boolean = !(dayOfTraining % 2 == 0)
+    val isEven = dayOfTraining % 2 == 0
 
-    var todayTraining = """Упражнения для рук: $hands
-       |Упражнения для ног: $legs
-       |Упражнения для спины: $back
-       |Упражнения для пресса: $press
+    val todayTraining = """Упражнения для рук: ${!isEven}
+       |Упражнения для ног: $isEven
+       |Упражнения для спины: $isEven
+       |Упражнения для пресса: ${!isEven}
    """.trimMargin()
 
     println(todayTraining)
