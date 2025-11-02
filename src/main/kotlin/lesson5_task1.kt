cdc7aa82f6536d69d562f09fc727cb510dedc766
shvedjr <shvedjr@gmail.com>
2025-11-02 22:10:47 +0300

    KS-5-1-task1

--- src/main/kotlin/lesson5_task1.kt
+++ src/main/kotlin/lesson5_task1.kt
@@ -2,7 +2,7 @@ package org.example.lesson_5
 
 fun main() {
 
-    print("Введи результат 3 + 4 = ")
+    print("Введи результат: 3 + 4 = ")
     val captcha = readln().toInt()
 
     if (captcha == 7) {
