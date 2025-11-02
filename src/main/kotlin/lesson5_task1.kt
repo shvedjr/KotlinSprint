546e75e8301228d2f9e4b02c9d6fbbecb2269d88
shvedjr <shvedjr@gmail.com>
2025-11-02 22:06:12 +0300

    KS-5-1-task1

---
+++ src/main/kotlin/lesson5_task1.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1
+
+fun main() {
+
+    print("Введи результат 3 + 4 = ")
+    val captcha = readln().toInt()
+
+    if (captcha == 7) {
+        println("Добро подаловать!")
+    } else {
+        println("Доступ запрещён!")
+    }
+
+}
+
+const val AGE_OF_MAJORITY = 18
