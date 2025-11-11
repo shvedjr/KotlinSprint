963ad8775c1fe2527b33b94847a0d397e1d69b80
shvedjr <shvedjr@gmail.com>
2025-11-11 15:19:13 +0300

    KS-6-4-task4

---
+++ src/main/kotlin/lesson6_task4kt.kt
@@ -0,0 +1,21 @@
+import kotlin.random.Random
+
+fun main() {
+    val randomNumber = (1..9).random()
+    var userTries = 5
+
+    while (userTries > 0) {
+        print("Попытка ${6 - userTries}/5: ")
+        val userInput = readln()
+
+        if (userInput.toInt() == randomNumber) {
+            println("Это была великолепная игра!")
+            return
+        }
+
+        userTries--
+        println("Неверно")
+    }
+
+    println("Было загадано число $randomNumber")
+}
